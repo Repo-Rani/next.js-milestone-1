f118450a23dddf42fafb8b546b618d6f4a13643a
Miss_Rani <160070498+Repo-Rani@users.noreply.github.com>
2024-11-10 15:23:55 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+  return(
+  <div className="flex justify-center items-center  font-extrabold text-4xl bg-blue-950 text-white text-center">Hello World</div>
+
+    
+  )
+}
